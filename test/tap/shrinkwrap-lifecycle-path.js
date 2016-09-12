a90a19a81f04ea86a131e0adc69f4d1e15beb214
Daniel Stockman <daniels@zillow.com>
2016-09-12 15:36:35 -0700

    demonstrate incorrect CWD and PATH with failing test

---
+++ test/tap/shrinkwrap-lifecycle-path.js
@@ -0,0 +1,102 @@
+'use strict'
+var fs = require('graceful-fs')
+var path = require('path')
+var test = require('tap').test
+var Tacks = require('tacks')
+var File = Tacks.File
+var Dir = Tacks.Dir
+var Symlink = Tacks.Symlink
+var common = require('../common-tap.js')
+var testdir = path.join(__dirname, path.basename(__filename, '.js'))
+
+var fixture = new Tacks(Dir({
+  node_modules: Dir({
+    '.bin': Dir({
+      'echoPATH': Symlink('../a/bin.js')
+    }),
+    'a': Dir({
+      'bin.js': File([
+        '#!/usr/bin/env node',
+        'console.log("PATH from local bin")',
+        'console.log(process.env.PATH)'
+      ].join('\n')),
+      'package.json': File({
+        _requested: {
+          rawSpec: 'file:///mods/a'
+        },
+        name: 'a',
+        version: '1.0.0',
+        bin: {
+          'echoPATH': './bin.js'
+        }
+      })
+    })
+  }),
+  scripts: Dir({
+    echoCWD: File([
+      '#!/usr/bin/env node',
+      'console.log(process.cwd())',
+      'console.log("CWD from local script")'
+    ].join('\n'))
+  }),
+  'package.json': File({
+    name: 'shrinkwrap-lifecycle-path',
+    version: '1.0.0',
+    scripts: {
+      preshrinkwrap: './scripts/echoCWD',
+      shrinkwrap: process.platform === 'win32'
+        ? 'echo %CD%; echo %PATH%'
+        : 'echo $PWD; echo $PATH',
+      postshrinkwrap: 'echoPATH'
+    },
+    dependencies: {
+      'a': 'file:///mods/a'
+    },
+  })
+}))
+
+function setup () {
+  cleanup()
+  fixture.create(testdir)
+  fs.chmodSync(path.resolve(testdir, './node_modules/a/bin.js'), '0755')
+  fs.chmodSync(path.resolve(testdir, './scripts/echoCWD'), '0755')
+}
+
+function cleanup () {
+  fixture.remove(testdir)
+}
+
+test('setup', function (t) {
+  setup()
+  t.done()
+})
+
+test('verify CWD and PATH are correct during shrinkwrap lifecycle', function (t) {
+  common.npm([
+    'shrinkwrap',
+    '--loglevel', 'silent'
+  ], {
+    cwd: testdir
+  }, function (err, code, stdout, stderr) {
+    if (err) throw err
+    t.is(code, 0, 'exited ok')
+    t.notOk(stderr, 'no output stderr')
+    t.comment(stdout.trim())
+
+    var expectWorkingDir = testdir.toLowerCase()
+    var actualWorkingDir = stdout.trim().split('\n').shift().toLowerCase()
+    t.contains(actualWorkingDir, expectWorkingDir, 'cwd is package root')
+
+    var expectBinPath = path.resolve(testdir, 'node_modules', '.bin').toLowerCase()
+    var actualBinPath = stdout.trim().split('\n').pop().toLowerCase()
+    t.contains(actualBinPath, expectBinPath, 'path includes local .bin')
+
+    t.done()
+  })
+})
+
+test('cleanup', function (t) {
+  cleanup()
+  t.done()
+})
+
